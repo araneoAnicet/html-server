fc6d2384341222b1963e474e434ae42539672fd3
araneoAnicet <araneoanicet@gmail.com>
2020-02-10 22:47:16 +0100

    add socket structure generator

--- main.c
+++ main.c
@@ -5,7 +5,19 @@
 #include <netinet/in.h>
 
 
+typedef struct sockaddr_in socket_address;
 
+socket_address generate_socket_structure(
+    sa_family_t address_family,
+    int port,
+    const char* network_address
+    ) {
+        socket_address socket_info;
+        socket_info.sin_family = address_family;
+        socket_info.sin_port = htons(port);
+        inet_aton(network_address, &socket_info.sin_addr.s_addr);
+        return socket_info;
+    };
 
 int main() {
     return 0;
