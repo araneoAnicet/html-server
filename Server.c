c3f45edc03f75e8f875bae7c853d77a86380e314
araneoAnicet <araneoanicet@gmail.com>
2020-02-21 20:24:02 +0100

    create separate files

---
+++ Server.c
@@ -0,0 +1,73 @@
+#include "Server.h"
+
+FILE* getHTMLDocument(char* fileName) {
+    return fopen(fileName, "r");
+}
+
+struct sockaddr_in generate_socket_structure(
+    short address_family,
+    unsigned short port,
+    const char* network_address
+    ) {
+        struct sockaddr_in socket_info;
+        socket_info.sin_family = address_family;
+        socket_info.sin_port = htons(port);
+        inet_aton(network_address, &socket_info.sin_addr.s_addr);
+        return socket_info;
+    };
+
+int create_socket(struct sockaddr_in socket_info, int protocol_index) {
+    int socket_file_descriptor = socket(socket_info.sin_family, protocol_index, 0);
+    if (socket_file_descriptor != -1) {
+        printf("Binding socket...\n");
+        bind(socket_file_descriptor, (struct sockaddr*) &socket_info, sizeof(socket_info));
+        printf("New socket binded at %u:%d\n", socket_info.sin_addr.s_addr, socket_info.sin_port);
+        return socket_file_descriptor;
+    } else {
+        printf("FAILED SOCKET CREATING\n");
+    }
+}
+
+void run(int socket_file_descriptor, int max_requests, char* responseHeader, int sizeOfResponseHeaders,  FILE* htmlDocument) {
+    int listening = listen(socket_file_descriptor, max_requests);
+    if (listening == 0) {
+        printf("Listening %d connections on socket...\n", max_requests);
+        int client_socket_file_descriptor;
+        short receive_buffer_size = 8192;
+        char receive_buffer[receive_buffer_size];
+
+        int responseSize;
+        char* buffer;
+        long fileSize;
+        fseek(htmlDocument, 0, SEEK_END);
+        fileSize = ftell(htmlDocument);
+        fseek(htmlDocument, 0, SEEK_SET);
+        buffer = malloc(fileSize);
+        fread(buffer, 1, fileSize, htmlDocument);
+        responseSize = sizeOfResponseHeaders + sizeof(const char) * 2 + fileSize;
+        printf("RESPONSE SIEZE: %li BYTES\n", responseSize);
+        char response[responseSize];
+
+        strcpy(response, responseHeader);
+        strcat(response, "\n\n");
+        strcat(response, buffer);
+
+        free(buffer);
+        fclose(htmlDocument);
+        while (1) {
+            client_socket_file_descriptor = accept(socket_file_descriptor, NULL, NULL);
+            printf("ACCEPTED NEW CONNECTION!\n");
+            int received_bytes = read(client_socket_file_descriptor, receive_buffer, receive_buffer_size);
+
+            printf("Received data:\n%s", receive_buffer);
+            printf("SENDING RESPONSE......\n%s", response);
+            send(client_socket_file_descriptor, response, responseSize, 0);
+            close(client_socket_file_descriptor);
+            printf("CONNECTION CLOSED!\n");
+           
+        }
+    } else {
+        printf("FAILED SOCKET LISTENING\n");
+    }
+
+}
